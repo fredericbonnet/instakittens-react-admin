9ebceeb849b841306c187808dfa626ef402b07ec
Frederic Bonnet <fredericbonnet@free.fr>
2019-02-14 17:12:35 +0100

    Upgraded Cypress and TestCafé to latest versions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,1 @@
 /// <reference types="Cypress" />
-
-/*
- * Workaround for bug in cy.visit() when visiting the current URL. As our
- * routing is hash-based, we compare the current location hash to the URL to
- * visit, and if they are the same then we just yield the current window.
- */
-Cypress.Commands.overwrite('visit', (orig, url, options) => {
-  cy.location().then(location => {
-    if (location.hash === url) {
-      return orig(url + '?', options);
-    } else {
-      return orig(url, options);
-    }
-  });
-});
